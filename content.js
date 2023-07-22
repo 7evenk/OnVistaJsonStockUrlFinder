5ff6aa69e348620bafde3e0d5bbad9250a0bc264
7evenk <seven@sevenk.org>
2023-07-22 20:16:47 +0200

    Add sponsor links

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 window.addEventListener('message', function(event) {
-    if (event.data.type && event.data.type === 'ONVISTA_API_REQUEST') {
+  if (event.data.type && event.data.type === 'ONVISTA_API_REQUEST') {
       let overlay = document.createElement('div');
       overlay.style.position = 'fixed';
       overlay.style.left = '25%';
@@ -15,7 +15,7 @@ window.addEventListener('message', function(event) {
       overlay.style.borderRadius = '10px';
       overlay.innerText = 'OnVista API Request Detected:\n\n' + event.data.url;
       document.body.appendChild(overlay);
-  
+
       let closeButton = document.createElement('button');
       closeButton.style.position = 'absolute';
       closeButton.style.right = '10px';
@@ -30,18 +30,59 @@ window.addEventListener('message', function(event) {
       closeButton.style.textAlign = 'center';
       closeButton.innerText = 'X';
       overlay.appendChild(closeButton);
-  
+
       closeButton.addEventListener('click', function() {
-        document.body.removeChild(overlay);
+          document.body.removeChild(overlay);
       });
-  
+
       window.addEventListener('keydown', function(event) {
-        if (event.key === 'Escape') {
-          if (document.body.contains(overlay)) {
-            document.body.removeChild(overlay);
+          if (event.key === 'Escape') {
+              if (document.body.contains(overlay)) {
+                  document.body.removeChild(overlay);
+              }
           }
-        }
       });
-    }
-  });
-  
+
+      let heading = document.createElement('h2');
+      heading.style.position = 'absolute';
+      heading.style.bottom = '50px';
+      heading.style.left = '30%'; // Verschiebt die Überschrift etwas mehr in die Mitte
+      heading.style.color = 'white';
+      heading.style.textShadow = '2px 2px 4px rgba(0, 0, 0, 0.5)';
+      heading.style.fontFamily = 'Arial, sans-serif';
+      heading.style.fontSize = '24px';
+      heading.innerText = 'Sponsor Me';
+      overlay.appendChild(heading);
+
+      let linkContainer = document.createElement('div');
+      linkContainer.style.position = 'absolute';
+      linkContainer.style.bottom = '10px';
+      linkContainer.style.left = '50%';
+      linkContainer.style.transform = 'translateX(-50%)';
+      linkContainer.style.display = 'flex';
+      linkContainer.style.justifyContent = 'center';
+      overlay.appendChild(linkContainer);
+
+      let coffeeLink = document.createElement('a');
+      coffeeLink.href = 'https://www.buymeacoffee.com/svenknoerzer';
+      coffeeLink.target = '_blank'; // Öffnet den Link in einem neuen Fenster
+      let coffeeIcon = document.createElement('img');
+      coffeeIcon.src = 'https://www.buymeacoffee.com/assets/img/guidelines/download-assets-sm-1.svg'; // Ersetzen Sie dies durch Ihre tatsächliche URL
+      coffeeIcon.style.width = '150px';
+      coffeeIcon.style.height = 'auto';
+      coffeeLink.appendChild(coffeeIcon);
+      linkContainer.appendChild(coffeeLink);
+
+      let paypalLink = document.createElement('a');
+      paypalLink.href = 'https://www.paypal.com/paypalme/svenknoerzer';
+      paypalLink.target = '_blank'; // Öffnet den Link in einem neuen Fenster
+      let paypalIcon = document.createElement('img');
+      paypalIcon.src = 'https://www.paypalobjects.com/digitalassets/c/website/marketing/apac/C2/logos-buttons/optimize/44_Grey_PayPal_Pill_Button.png'; // Ersetzen Sie dies durch Ihre tatsächliche URL
+      paypalIcon.style.width = '134px';
+      paypalIcon.style.height = 'auto';
+      paypalLink.appendChild(paypalIcon);
+      linkContainer.appendChild(paypalLink);
+
+      coffeeLink.style.marginRight = '10px'; // Fügt einen Abstand zwischen den Icons hinzu
+  }
+});

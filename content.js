c1cd2a644dfd60674de6d7b867977b01394185fb
7evenk <seven@sevenk.org>
2023-07-24 08:29:46 +0200

    Improve styling

--- content.js
+++ content.js
@@ -11,7 +11,6 @@ switchOverlay.style.color = 'white';
 switchOverlay.style.fontSize = '14px';
 switchOverlay.style.padding = '5px';
 switchOverlay.style.zIndex = '10000';
-switchOverlay.style.border = '2px solid white';
 switchOverlay.style.borderRadius = '10px';
 switchOverlay.style.textAlign = 'center';
 switchOverlay.style.cursor = 'pointer';
@@ -36,54 +35,54 @@ window.addEventListener('message', function (event) {
     }
 
     let template = `
-      <div id="overlay" style="position: fixed; left: 25%; top: 25%; width: 50%; height: 50%; background-color: rgba(0, 0, 0, 0.8); color: white; font-size: 20px; padding: 20px; z-index: 10000; border: 2px solid white; border-radius: 10px; overflow: auto;">
-        <button style="position: absolute; right: 10px; top: 10px; font-size: 20px; color: white; background-color: black; border: none; border-radius: 50%; width: 30px; height: 30px; text-align: center;" onclick="document.body.removeChild(this.parentNode);">X</button>
-        <!--<div>
-          <h2 style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 24px;">OnVista API Request Detected</h2>
-          <div style="display: flex;">
-            <p style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px;">${event.data.url}</p>
-          </div>
-        </div>-->
+      <div id="overlay" style="position: fixed; left: 25%; top: 25%; width: 50%; height: 50%; background-color: rgba(0, 0, 0, 0.8); color: white; font-size: 20px; padding: 20px; z-index: 10000; border-radius: 10px;">
+      <h2 style="margin: 0; color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 24px;">onvista Json Stock Url Finder for Portfolio Performance</h2>  
+      <button style="position: absolute; right: 10px; top: 10px; font-size: 20px; color: white; background-color: black; border: none; border-radius: 50%; width: 30px; height: 30px; text-align: center;" onclick="document.body.removeChild(this.parentNode);">X</button>
+        
+        <div style="height: 70%; overflow: auto; padding-right: 20px;margin-top: 20px;">
+
+        
         <div>
-          <h2 style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 24px;">Portfolio Performance Stock URL</h2>
-          <div style="display: flex;">
-            <p style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px;">${ppUrl}</p>
-            <button id="copyButton" style="font-size: 20px; color: white; background-color: black; border: none; border-radius: 10px; text-align: center;" onclick="navigator.clipboard.writeText('${ppUrl}'); this.style.backgroundColor = 'green'; setTimeout(() => this.style.backgroundColor = 'black', 1000);">Copy</button>
+          <div style="display: grid; grid-template-columns: 20% 70% 10%; padding-top: 10px;">
+            <span style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px;">Kurs URL: </span>
+            <span style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px; text-align: center;">${ppUrl}</span>
+            <button style="font-size: 20px; color: white; background-color: black; border: none; border-radius: 10px; text-align: center;" onclick="navigator.clipboard.writeText(this.previousElementSibling.innerText); this.style.backgroundColor = 'green'; setTimeout(() => this.style.backgroundColor = 'black', 1000);">Copy</button>
+          </div> 
+          <div style="display: grid; grid-template-columns: 20% 70% 10%; padding-top: 10px;">
+            <span style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px;">Pfad zum Datum: </span>
+            <span style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px; text-align: center;">$.datetimeLast[*]</span>
+            <button style="font-size: 20px; color: white; background-color: black; border: none; border-radius: 10px; text-align: center;" onclick="navigator.clipboard.writeText(this.previousElementSibling.innerText); this.style.backgroundColor = 'green'; setTimeout(() => this.style.backgroundColor = 'black', 1000);">Copy</button>
+          </div> 
+          <div style="display: grid; grid-template-columns: 20% 70% 10%; padding-top: 10px;">
+            <span style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px;">Pfad zum Kurs: </span>
+            <span style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px; text-align: center;">$.last[*]</span>
+            <button style="font-size: 20px; color: white; background-color: black; border: none; border-radius: 10px; text-align: center;" onclick="navigator.clipboard.writeText(this.previousElementSibling.innerText); this.style.backgroundColor = 'green'; setTimeout(() => this.style.backgroundColor = 'black', 1000);">Copy</button>
           </div>
-        </div>
+          <div style="display: grid; grid-template-columns: 20% 70% 10%; padding-top: 10px;">
+            <span style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px;">Pfad zum Tagestief: </span>
+            <span style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px; text-align: center;">$.low[*]</span>
+            <button style="font-size: 20px; color: white; background-color: black; border: none; border-radius: 10px; text-align: center;" onclick="navigator.clipboard.writeText(this.previousElementSibling.innerText); this.style.backgroundColor = 'green'; setTimeout(() => this.style.backgroundColor = 'black', 1000);">Copy</button>
+          </div>
+          <div style="display: grid; grid-template-columns: 20% 70% 10%; padding-top: 10px;">
+            <span style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px;">Pfad zum Tageshoch: </span>
+            <span style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px; text-align: center;">$.high[*]</span>
+            <button style="font-size: 20px; color: white; background-color: black; border: none; border-radius: 10px; text-align: center;" onclick="navigator.clipboard.writeText(this.previousElementSibling.innerText); this.style.backgroundColor = 'green'; setTimeout(() => this.style.backgroundColor = 'black', 1000);">Copy</button>
+          </div>
+          <div style="display: grid; grid-template-columns: 20% 70% 10%; padding-top: 10px;">
+            <span style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px;">Pfad zum Volumen: </span>
+            <span style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px; text-align: center;">$.volume[*]</span>
+            <button style="font-size: 20px; color: white; background-color: black; border: none; border-radius: 10px; text-align: center;" onclick="navigator.clipboard.writeText(this.previousElementSibling.innerText); this.style.backgroundColor = 'green'; setTimeout(() => this.style.backgroundColor = 'black', 1000);">Copy</button>
+          </div>
+      </div>
+      
         <div>
-        <div style="display: flex; justify-content: space-between;">
-          <p style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px;">Pfad zum Datum: </p>
-          <p style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px;">$.datetimeLast[*]</p>
-          <button style="font-size: 20px; color: white; background-color: black; border: none; border-radius: 10px; text-align: center;" onclick="navigator.clipboard.writeText(this.previousElementSibling.innerText); this.style.backgroundColor = 'green'; setTimeout(() => this.style.backgroundColor = 'black', 1000);">Copy</button>
-        </div> 
-        <div style="display: flex; justify-content: space-between;">
-          <p style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px;">Pfad zum Kurs: </p>
-          <p style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px;">$.last[*]</p>
-          <button style="font-size: 20px; color: white; background-color: black; border: none; border-radius: 10px; text-align: center;" onclick="navigator.clipboard.writeText(this.previousElementSibling.innerText); this.style.backgroundColor = 'green'; setTimeout(() => this.style.backgroundColor = 'black', 1000);">Copy</button>
-        </div>
-        <div style="display: flex; justify-content: space-between;">
-          <p style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px;">Pfad zum Tagestief: </p>
-          <p style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px;">$.low[*]</p>
-          <button style="font-size: 20px; color: white; background-color: black; border: none; border-radius: 10px; text-align: center;" onclick="navigator.clipboard.writeText(this.previousElementSibling.innerText); this.style.backgroundColor = 'green'; setTimeout(() => this.style.backgroundColor = 'black', 1000);">Copy</button>
-        </div>
-        <div style="display: flex; justify-content: space-between;">
-          <p style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px;">Pfad zum Tageshoch: </p>
-          <p style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px;">$.high[*]</p>
-          <button style="font-size: 20px; color: white; background-color: black; border: none; border-radius: 10px; text-align: center;" onclick="navigator.clipboard.writeText(this.previousElementSibling.innerText); this.style.backgroundColor = 'green'; setTimeout(() => this.style.backgroundColor = 'black', 1000);">Copy</button>
-        </div>
-        <div style="display: flex; justify-content: space-between;">
-          <p style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px;">Pfad zum Volumen: </p>
-          <p style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px;">$.volume[*]</p>
-          <button style="font-size: 20px; color: white; background-color: black; border: none; border-radius: 10px; text-align: center;" onclick="navigator.clipboard.writeText(this.previousElementSibling.innerText); this.style.backgroundColor = 'green'; setTimeout(() => this.style.backgroundColor = 'black', 1000);">Copy</button>
-        </div>
-        </div>
-        <div style="position: absolute; bottom: 10px; left: 50%; transform: translateX(-50%);">
-          <div style="margin-left: -24px";>
-            <h2 style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px;">Sponsor Me</h2>
+          <div id="sponsorLinksContainer" style="position: absolute; bottom: 10px; left: 50%; transform: translateX(-50%);">
+            <div style="margin-left: -24px";>
+              <h2 style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; font-size: 18px;">Sponsor Me</h2>
+            </div>
+            <a href="https://www.buymeacoffee.com/svenknoerzer" target="_blank" style="margin-right: 10px;"><img src="https://www.buymeacoffee.com/assets/img/guidelines/download-assets-sm-1.svg" style="width: 150px; height: auto;"></a>
+            <a href="https://www.paypal.com/paypalme/svenknoerzer" target="_blank"><img src="https://www.paypalobjects.com/digitalassets/c/website/marketing/apac/C2/logos-buttons/optimize/44_Grey_PayPal_Pill_Button.png" style="width: 134px; height: auto;"></a>
           </div>
-          <a href="https://www.buymeacoffee.com/svenknoerzer" target="_blank" style="margin-right: 10px;"><img src="https://www.buymeacoffee.com/assets/img/guidelines/download-assets-sm-1.svg" style="width: 150px; height: auto;"></a>
-          <a href="https://www.paypal.com/paypalme/svenknoerzer" target="_blank"><img src="https://www.paypalobjects.com/digitalassets/c/website/marketing/apac/C2/logos-buttons/optimize/44_Grey_PayPal_Pill_Button.png" style="width: 134px; height: auto;"></a>
         </div>
       </div>
     `;
